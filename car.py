fdba0d8e1b9c28532f81e3124a6c685cf2f12e53
henryjdog <dr.henry.j.dog@gmail.com>
2018-10-05 14:39:31 -0400

    updated car module, tabs to spaces

--- car.py
+++ car.py
@@ -11,50 +11,51 @@ from controller import Controller
 
 imu = IMU()
 try:
-	controller = Controller()
+    controller = Controller()
 except:
-	controller = None
+    controller = None
 
 # Main event loop
 while True:
     print imu.read()
     sleep(0.5)
-	if controller:
-		evbuf = jsdev.read(8)
-		if evbuf:
-			time, value, type, number = struct.unpack('IhBB', evbuf)
+    
+    if controller:
+        evbuf = jsdev.read(8)
+        if evbuf:
+            time, value, type, number = struct.unpack('IhBB', evbuf)
 
-			if type & 0x80:
-				 print "(initial)",
+            if type & 0x80:
+                 print "(initial)",
 
-			if type & 0x01:
-				button = button_map[number]
-				if button:
-					button_states[button] = value
-					if value:
-						print "%s pressed" % (button)
-					else:
-						print "%s released" % (button)
+            if type & 0x01:
+                button = button_map[number]
+                if button:
+                    button_states[button] = value
+                    if value:
+                        print "%s pressed" % (button)
+                    else:
+                        print "%s released" % (button)
 
-			if type & 0x02:
-				axis = axis_map[number]
-				if axis:
-					fvalue = value / 32767.0
-					axis_states[axis] = fvalue
-					print "%s: %.3f" % (axis, fvalue)
-					if axis == 'x' or axis == 'hat0x':
-						if fvalue > 0.01:
-							r(fvalue)
-						elif fvalue < -0.01:
-							l(-fvalue)
-						else:
-							stop([lpwm, rpwm])
-					if axis == 'y' or axis == 'hat0y':
-						if fvalue < -0.01:
-							f(-fvalue)
-						elif fvalue > 0.01:
-							b(fvalue)
-						else:
-							stop([fpwm, bpwm])
+            if type & 0x02:
+                axis = axis_map[number]
+                if axis:
+                    fvalue = value / 32767.0
+                    axis_states[axis] = fvalue
+                    print "%s: %.3f" % (axis, fvalue)
+                    if axis == 'x' or axis == 'hat0x':
+                        if fvalue > 0.01:
+                            r(fvalue)
+                        elif fvalue < -0.01:
+                            l(-fvalue)
+                        else:
+                            stop([lpwm, rpwm])
+                    if axis == 'y' or axis == 'hat0y':
+                        if fvalue < -0.01:
+                            f(-fvalue)
+                        elif fvalue > 0.01:
+                            b(fvalue)
+                        else:
+                            stop([fpwm, bpwm])
 
-		
+        
